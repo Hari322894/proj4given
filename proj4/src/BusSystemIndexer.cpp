83641252ac2737e8b3a5629c6948c5f9943a237a
Hari322894 <hmaddineni@ucdavis.edu>
2025-03-07 19:57:05 -0800

    1

--- proj4/src/BusSystemIndexer.cpp
+++ proj4/src/BusSystemIndexer.cpp
@@ -1,6 +1,17 @@
 #include "BusSystemIndexer.h"
+#include "GeographicUtils.h"
 #include <algorithm>
 #include <unordered_map>
+#include <unordered_set>
+#include <memory>
+
+// Define PairHash to be used in unordered_map
+struct PairHash {
+    template <class T1, class T2>
+    std::size_t operator()(const std::pair<T1, T2>& pair) const {
+        return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
+    }
+};
 
 struct CBusSystemIndexer::SImplementation {
     std::shared_ptr<CBusSystem> DBusSystem;
