b839fafb229035f4d17610760a03bf7bf09438b9
Hari322894 <hmaddineni@ucdavis.edu>
2025-03-08 17:25:24 -0800

    1

--- proj4/include/BusSystemIndexer.h
+++ proj4/include/BusSystemIndexer.h
@@ -1,33 +1,26 @@
 #ifndef BUSSYSTEMINDEXER_H
 #define BUSSYSTEMINDEXER_H
-
-#include <memory>
-#include <unordered_map>
-#include <unordered_set>
-#include <vector>
 #include "BusSystem.h"
-#include "StreetMap.h"
+#include <unordered_set>
 
-class CBusSystemIndexer {
-public:
-    using TNodeID = CStreetMap::TNodeID;
-    using SStop = CBusSystem::SStop;
-    using SRoute = CBusSystem::SRoute;
+class CBusSystemIndexer{
+    private:
+        struct SImplementation;
+        std::unique_ptr<SImplementation> DImplementation;
+    public:
+        using TNodeID = CStreetMap::TNodeID;
+        using SStop = CBusSystem::SStop;
+        using SRoute = CBusSystem::SRoute;
+        CBusSystemIndexer(std::shared_ptr<CBusSystem> bussystem);
+        ~CBusSystemIndexer();
 
-    CBusSystemIndexer(std::shared_ptr<CBusSystem> busSystem);
-    ~CBusSystemIndexer();
-
-    std::size_t StopCount() const noexcept;
-    std::size_t RouteCount() const noexcept;
-    std::shared_ptr<SStop> SortedStopByIndex(std::size_t index) const noexcept;
-    std::shared_ptr<SRoute> SortedRouteByIndex(std::size_t index) const noexcept;
-    std::shared_ptr<SStop> StopByNodeID(TNodeID id) const noexcept;
-    bool RoutesByNodeIDs(TNodeID src, TNodeID dest, std::unordered_set<std::shared_ptr<SRoute>> &routes) const noexcept;
-    bool RouteBetweenNodeIDs(TNodeID src, TNodeID dest) const noexcept;
-
-private:
-    struct SImplementation;
-    std::unique_ptr<SImplementation> DImplementation;
+        std::size_t StopCount() const noexcept;
+        std::size_t RouteCount() const noexcept;
+        std::shared_ptr<SStop> SortedStopByIndex(std::size_t index) const noexcept;
+        std::shared_ptr<SRoute> SortedRouteByIndex(std::size_t index) const noexcept;
+        std::shared_ptr<SStop> StopByNodeID(TNodeID id) const noexcept;
+        bool RoutesByNodeIDs(TNodeID src, TNodeID dest, std::unordered_set<std::shared_ptr<SRoute> > &routes) const noexcept;
+        bool RouteBetweenNodeIDs(TNodeID src, TNodeID dest) const noexcept;
 };
 
 #endif
