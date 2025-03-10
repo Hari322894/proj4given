4579816427749c1d16e3cef390db17ac4fea1c31
Hari322894 <hmaddineni@ucdavis.edu>
2025-03-09 17:56:14 -0700

    1

--- proj4/src/DijkstraTransportationPlannerCommandLine.cpp
+++ proj4/src/DijkstraTransportationPlannerCommandLine.cpp
@@ -1,26 +1,40 @@
-#include "TransportationPlannerCommandLine.h"
+#include "TransportationPlannerCommandLine.h"  // Include the correct header
+#include "TransportationPlannerConfig.h"
 
 struct CTransportationPlannerCommandLine::SImplementation{
-
-    SImplementation(std::shared_ptr<CDataSource> cmdsrc, std::shared_ptr<CDataSink> outsink, std::shared_ptr<CDataSink> errsink, std::shared_ptr<CDataFactory> results, std::shared_ptr<CTransportationPlanner> planner){
-
-    };
+    std::shared_ptr<CDataSource> CommandSource;
+    std::shared_ptr<CDataSink> OutputSink;
+    std::shared_ptr<CDataSink> ErrorSink;
+    std::shared_ptr<CDataFactory> ResultFactory;
+    std::shared_ptr<CTransportationPlanner> Planner;
     
-    bool ProcessCommands(){
-
-    };
+    SImplementation(std::shared_ptr<CDataSource> cmdsrc, 
+                   std::shared_ptr<CDataSink> outsink, 
+                   std::shared_ptr<CDataSink> errsink, 
+                   std::shared_ptr<CDataFactory> results, 
+                   std::shared_ptr<CTransportationPlanner> planner) 
+        : CommandSource(cmdsrc), OutputSink(outsink), ErrorSink(errsink), 
+          ResultFactory(results), Planner(planner) {
+    }
+   
+    bool ProcessCommands() {
+        // Implement your command processing logic here
+        return true; // Return appropriate value based on your logic
+    }
 };
-        
-        
-CTransportationPlannerCommandLine::CTransportationPlannerCommandLine(std::shared_ptr<CDataSource> cmdsrc, std::shared_ptr<CDataSink> outsink, std::shared_ptr<CDataSink> errsink, std::shared_ptr<CDataFactory> results, std::shared_ptr<CTransportationPlanner> planner){
+       
+CTransportationPlannerCommandLine::CTransportationPlannerCommandLine(std::shared_ptr<CDataSource> cmdsrc, 
+                                                                    std::shared_ptr<CDataSink> outsink, 
+                                                                    std::shared_ptr<CDataSink> errsink, 
+                                                                    std::shared_ptr<CDataFactory> results, 
+                                                                    std::shared_ptr<CTransportationPlanner> planner) {
     DImplementation = std::make_unique<SImplementation>(cmdsrc, outsink, errsink, results, planner);
-};
+}
 
-CTransportationPlannerCommandLine::~CTransportationPlannerCommandLine(){
+CTransportationPlannerCommandLine::~CTransportationPlannerCommandLine() {
+    // Destructor implementation (already correct in your code)
+}
 
-};
-
-bool CTransportationPlannerCommandLine::ProcessCommands(){
+bool CTransportationPlannerCommandLine::ProcessCommands() {
     return DImplementation->ProcessCommands();
-};
-
+}
