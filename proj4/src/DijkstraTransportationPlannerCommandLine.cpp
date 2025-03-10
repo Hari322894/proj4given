759bd9b8d7970effa73836bc5ee77dd32a23c9dd
Hari322894 <hmaddineni@ucdavis.edu>
2025-03-09 19:43:09 -0700

    1

---
+++ proj4/src/DijkstraTransportationPlannerCommandLine.cpp
@@ -0,0 +1,26 @@
+#include "TransportationPlannerConfig.h"
+
+struct CTransportationPlannerCommandLine::SImplementation{
+
+    SImplementation(std::shared_ptr<CDataSource> cmdsrc, std::shared_ptr<CDataSink> outsink, std::shared_ptr<CDataSink> errsink, std::shared_ptr<CDataFactory> results, std::shared_ptr<CTransportationPlanner> planner){
+
+    };
+    
+    bool ProcessCommands(){
+
+    };
+};
+        
+        
+CTransportationPlannerCommandLine::CTransportationPlannerCommandLine(std::shared_ptr<CDataSource> cmdsrc, std::shared_ptr<CDataSink> outsink, std::shared_ptr<CDataSink> errsink, std::shared_ptr<CDataFactory> results, std::shared_ptr<CTransportationPlanner> planner){
+    DImplementation = std::make_unique<SImplementation>(cmdsrc, outsink, errsink, results, planner);
+};
+
+CTransportationPlannerCommandLine::~CTransportationPlannerCommandLine(){
+
+};
+
+bool CTransportationPlannerCommandLine::ProcessCommands(){
+    return DImplementation->ProcessCommands();
+};
+
