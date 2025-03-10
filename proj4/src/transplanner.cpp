5313e3826d6d968453ba1618847c73021e578a34
Hari322894 <hmaddineni@ucdavis.edu>
2025-03-09 20:40:55 -0700

    1

--- proj4/src/transplanner.cpp
+++ proj4/src/transplanner.cpp
@@ -1,12 +1,17 @@
 #include "TransportationPlannerCommandLine.h"
+#include "FileDataSource.h"
+#include "FileDataSink.h"
+#include "FileDataFactory.h"
+#include "DijkstraTransportationPlanner.h"
 #include <memory>
 
 int main(int argc, char *argv[]) {
-    auto cmdsrc = std::make_shared<CDataSource>();
-    auto outsink = std::make_shared<CDataSink>();
-    auto errsink = std::make_shared<CDataSink>();
-    auto results = std::make_shared<CDataFactory>();
-    auto planner = std::make_shared<CTransportationPlanner>();
+    auto cmdsrc = std::make_shared<FileDataSource>("input.txt");
+    auto outsink = std::make_shared<FileDataSink>("output.txt");
+    auto errsink = std::make_shared<FileDataSink>("error.txt");
+    auto results = std::make_shared<FileDataFactory>();
+    auto config = std::make_shared<STransportationPlannerConfig>();
+    auto planner = std::make_shared<CDijkstraTransportationPlanner>(config);
 
     CTransportationPlannerCommandLine commandLine(cmdsrc, outsink, errsink, results, planner);
     if (!commandLine.ProcessCommands()) {
