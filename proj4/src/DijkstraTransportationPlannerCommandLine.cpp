fa918d14b38da8a0837573e2708d9f732fe56f64
Hari322894 <hmaddineni@ucdavis.edu>
2025-03-09 18:32:16 -0700

    1

--- proj4/src/DijkstraTransportationPlannerCommandLine.cpp
+++ proj4/src/DijkstraTransportationPlannerCommandLine.cpp
@@ -1,36 +1,26 @@
-#include "TransportationPlannerCommandLine.h"
-#include <memory>
+#include "TransportationPlannerConfig.h"
 
 struct CTransportationPlannerCommandLine::SImplementation{
-    std::shared_ptr<CDataSource> DCommandSource;
-    std::shared_ptr<CDataSink> DOutputSink;
-    std::shared_ptr<CDataSink> DErrorSink;
-    std::shared_ptr<CDataFactory> DResultsFactory;
-    std::shared_ptr<CTransportationPlanner> DPlanner;
+
+    SImplementation(std::shared_ptr<CDataSource> cmdsrc, std::shared_ptr<CDataSink> outsink, std::shared_ptr<CDataSink> errsink, std::shared_ptr<CDataFactory> results, std::shared_ptr<CTransportationPlanner> planner){
+
+    };
     
-    SImplementation(std::shared_ptr<CDataSource> cmdsrc, 
-                   std::shared_ptr<CDataSink> outsink, 
-                   std::shared_ptr<CDataSink> errsink, 
-                   std::shared_ptr<CDataFactory> results, 
-                   std::shared_ptr<CTransportationPlanner> planner)
-        : DCommandSource(cmdsrc), DOutputSink(outsink), DErrorSink(errsink),
-          DResultsFactory(results), DPlanner(planner) {
-    }
-   
     bool ProcessCommands(){
-        // Implementation here
-        return true; // Or actual implementation return value
-    }
+
+    };
 };
-       
+        
+        
 CTransportationPlannerCommandLine::CTransportationPlannerCommandLine(std::shared_ptr<CDataSource> cmdsrc, std::shared_ptr<CDataSink> outsink, std::shared_ptr<CDataSink> errsink, std::shared_ptr<CDataFactory> results, std::shared_ptr<CTransportationPlanner> planner){
     DImplementation = std::make_unique<SImplementation>(cmdsrc, outsink, errsink, results, planner);
-}
+};
 
 CTransportationPlannerCommandLine::~CTransportationPlannerCommandLine(){
-    // Destructor implementation
-}
+
+};
 
 bool CTransportationPlannerCommandLine::ProcessCommands(){
     return DImplementation->ProcessCommands();
-}
+};
+
